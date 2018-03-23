a37f1584d69a68399e7cd479e90daf0eb29df4e1
Basith Hameem <basithhameem@gmail.com>
2018-03-24 00:40:54 +0530

    New files added

--- lottery_web/agent_dashboard.php
+++ lottery_web/agent_dashboard.php
@@ -1,4 +1,5 @@
 <?php
    include("session_admin.php");
    echo "You reached agent dashboard";
+   echo "<a href='agent_upload.php'>Upload Lottery Image</a>";
 ?>
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Import the dot sql file into your local host. 
+change the credentials
+give full permission to your folders and subfolders for proper working.
